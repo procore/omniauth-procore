fd150d6ee755d5e1b7e02cb2312db068d14ee3a2
Gedward Gonzalez <gonzalezgedward@gmail.com>
2017-11-29 14:54:11 -0800

    Override the callback_url in the procore strategy
    OmniAuth 1.4.0 deleted the override of callback_url from
    OmniAuth::Strategies::OAuth2.
    
    See https://github.com/omniauth/omniauth-oauth2/commit/26152673224aca5c3e918bcc83075dbb0659717f and https://github.com/omniauth/omniauth-oauth2/issues/81
    
    This causes an invalid grant error due to a uri mismatch.

--- lib/omniauth/strategies/procore.rb
+++ lib/omniauth/strategies/procore.rb
@@ -24,6 +24,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('/vapid/me').parsed
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
